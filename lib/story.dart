29139df1e42cb2161ffd503df389dc7181602aed
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-03-28 22:57:22 +0400

    init commit

---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+class Story {
+  late String storyTitle;
+  late String choiceOne;
+  late String choiceTwo;
+
+  Story({required String storyTitle,required String choice1,required String choice2}) {
+    storyTitle = storyTitle;
+    choiceOne = choice1;
+    choiceTwo = choice2;
+  }
+
+}
